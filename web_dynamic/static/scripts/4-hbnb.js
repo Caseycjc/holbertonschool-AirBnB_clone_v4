a5fb0aaca893cbeea44a276be92d893ad8022286
linszay <lindseyjlancaster@gmail.com>
2023-04-19 12:20:20 -0500

    added files for task 4

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+    let amenityIds = {};
+  
+    // update amenityIds on checkbox change
+    $('input[type=checkbox]').change(function () {
+      const amenityId = $(this).data('id');
+      const amenityName = $(this).data('name');
+      if (this.checked) {
+        amenityIds[amenityId] = amenityName;
+      } else {
+        delete amenityIds[amenityId];
+      }
+      const amenitiesList = Object.values(amenityIds).join(', ');
+      $('.amenities h4').text(amenitiesList);
+    });
+  
+    // make new POST request on button click
+    $('button').click(function () {
+      const placesUrl = 'http://0.0.0.0:5001/api/v1/places_search/';
+      const requestData = { amenities: Object.keys(amenityIds) };
+  
+      fetch(placesUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestData)
+      })
+      .then(response => response.json())
+      .then(data => {
+        const placesSection = document.querySelector('section.places');
+        placesSection.innerHTML = ''; // clear existing results
+        for (const place of data) {
+          const article = document.createElement('article');
+          article.innerHTML = `
+            <div class="title_box">
+              <h2>${place.name}</h2>
+              <div class="price_by_night">$${place.price_by_night}</div>
+            </div>
+            <div class="information">
+              <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+              <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+              <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+            </div>
+            <div class="description">
+              ${place.description}
+            </div>
+          `;
+          placesSection.appendChild(article);
+        }
+      })
+      .catch(error => console.error('Error:', error));
+    });
+  
+    // check API status on page load
+    const apiUrl = 'http://0.0.0.0:5001/api/v1/status/';
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        if (data.status === 'OK') {
+          const apiStatusDiv = document.querySelector('#api_status');
+          apiStatusDiv.classList.add('available');
+        } else {
+          const apiStatusDiv = document.querySelector('#api_status');
+          apiStatusDiv.classList.remove('available');
+        }
+      })
+      .catch(error => console.error('Error:', error));
+  });
+  
